c70b6e9545fe3fd2a3c26ba4391a0ec146027c60
Alex Gonzales <agonz056@ucr.edu>
2013-08-20 14:34:06 -0700

    word-count: iteration 2

--- javascript/word-count/words.js
+++ javascript/word-count/words.js
@@ -1,24 +1,18 @@
+/* in this implementation, a phrase is a string of words
+delimited by whitespace and each word is made up of alphabetic
+characters: 
+
+returns an array of lowercase words from the phrase with all 
+non-alphanumeric characters removed from the original phrase */
+var getWordList = function(phrase){
+	return phrase.toLowerCase().split(/\W+/g);
+};
+
 /* returns an object whose properties of 
 	are words from a phrase and their values are
 	occurrences of that word in the phrase */
 var Words = function(phrase) {
 
-	/* in this implementation, a phrase is a string of words
-	delimited by whitespace and each word is made up of alphabetic
-	characters: 
-
-	returns an array of lowercase words from the phrase with all 
-	non-alphanumeric characters removed from the original phrase */
-	var getWordList = function(phrase){
-
-		return removeNonAlpha(phrase).toLowerCase().split(/\s+/g);
-	};
-
-	/* returns a string with all non-alphanumeric/whitespace
-     characters removed from the original string. */
-	var removeNonAlpha = function(myString){
-		return myString.replace(/[^a-z1-9\s]/gi, '');
-	};
 
 	var myWordList = getWordList(phrase),
 		wordCount = {},
