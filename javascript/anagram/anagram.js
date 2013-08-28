1c29bb0d010ef6d098976440a1d8f9f2db0c08e6
Alex Gonzales <agonz056@ucr.edu>
2013-08-27 17:50:52 -0700

    anagram: iteration 3

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,25 +1,18 @@
-/* returns a lowercase, "sorted" string by 
-transforming myString into a character array, 
-sorting it and "casting" it into a string again */
-var toSortedString = function(myString){
-	return myString.toLowerCase().split('').sort().toString();
+var toSortedString = function toSortedStringFn (myString){
+    return myString.toLowerCase().split('').sort().toString();
 };
 
-var Anagram = function(word){
+var Anagram = function AnagramFn (word){
 
-	// returns a list of anagrams of word from candidateList
-	this.match = function(candidateList){
-		return candidateList.filter(isAnagram);
-	};
+    this.match = function matchFn (candidateList){
+        return candidateList.filter(isAnagram);
+    };
 
-	// the "sorted" string version of word 
-	var sortedWord = toSortedString(word);
+    var sortedWord = toSortedString(word);
 
-	/* candidate is an anagram iff it is in the set of 
-	permutations of word, however this includes word itself */
-	var isAnagram = function(candidate){
-		return toSortedString(candidate) === sortedWord;
-	};
+    var isAnagram = function isAnagramFn (candidate){
+        return toSortedString(candidate) === sortedWord;
+    };
 };
 
 module.exports = Anagram;
